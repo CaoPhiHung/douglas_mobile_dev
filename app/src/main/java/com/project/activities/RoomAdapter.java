75ce3c88a728ba173452d1eef46022418c26367b
Phi Hung Cao <caophihung8392@gmail.com>
2018-07-10 11:28:21 -0700

    local

---
+++ app/src/main/java/com/project/activities/RoomAdapter.java
@@ -0,0 +1,50 @@
+package com.project.activities;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 300284134 on 6/25/2018.
+ */
+
+public class RoomAdapter extends BaseAdapter {
+
+    Activity context;
+
+    public RoomAdapter()
+    {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        view = inflater.inflate(R.layout.custom_row, null);
+
+        return view;
+    }
+}
--- app/src/main/java/com/project/activities/UserInfoActivity.java
+++ app/src/main/java/com/project/activities/UserInfoActivity.java
@@ -9,12 +9,13 @@ public class UserInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_port);
 
-//        Bundle b = getIntent().getExtras();
-//        int id = b.getInt("id");
-//
-//        TextView tv = findViewById(R.id.txtTest);
-//        tv.setText("You have just open port id #" + id);
+        Bundle b = getIntent().getExtras();
+        int id = b.getInt("id");
+
+        TextView tv = findViewById(R.id.txtTest);
+        tv.setText("You have just open port id #" + id);
     }
 }
